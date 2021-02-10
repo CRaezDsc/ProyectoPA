d639524c3682116d888e6aa9a9b3f63ebfd41643
Carlos Raez <raez.isil@gmail.com>
2021-02-10 08:17:34 -0500

    Clase 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Alumno {
+  codigo: number;
+  nombre: string;
+  curso: string;
+  edad: number;
+}
+
+let listaAlumnos: Alumno[] = [
+  {codigo: 1, nombre: 'Carlos', curso: 'PA - I', edad: 20},
+  {codigo: 2, nombre: 'Oscar', curso: 'PA - I', edad: 20},
+  {codigo: 3, nombre: 'Guillermo', curso: 'PA - I', edad: 20},
+  {codigo: 4, nombre: 'Roy', curso: 'PA - I', edad: 20},
+ ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectoPa';
+
+  displayedColumns: string[] = ['codigo', 'nombre', 'curso', 'edad'];
+  dataSource = listaAlumnos;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
